05c52945e7d38e05aca26e0f45f32c13ce04ca2f
Dan Wilder <12650041+WilderDan@users.noreply.github.com>
2023-09-07 18:19:22 -0500

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  const getAge = (person) =>
+    (person.yearOfDeath ?? new Date().getFullYear()) - person.yearOfBirth;
 
+  return people.reduce(
+    (oldest, person) => (getAge(person) > getAge(oldest) ? person : oldest),
+    people[0]
+  );
 };
 
 // Do not edit below this line
