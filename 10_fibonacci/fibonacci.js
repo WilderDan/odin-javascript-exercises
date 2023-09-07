e0e446c2dbe6c722668b352c9bc0daf2539d393e
Dan Wilder <12650041+WilderDan@users.noreply.github.com>
2023-09-07 17:48:56 -0500

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function (position) {
+  if (position <= 0) return "OOPS";
 
+  let sequence = [1, 1];
+  for (let i = 2; i < position; ++i) {
+    const oldValue = sequence[1];
+    sequence[1] += sequence[0];
+    sequence[0] = oldValue;
+  }
+
+  return sequence[1];
 };
 
 // Do not edit below this line
