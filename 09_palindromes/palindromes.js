0a4d9e1dead6afe26b60d9010b694b44b4614ccc
Dan Wilder <12650041+WilderDan@users.noreply.github.com>
2023-09-07 16:41:25 -0500

    Complete exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  let standardForm = str
+    .toLowerCase()
+    .replace(/[.,\/#!$%\^&\*;:{}=\-_`~() ]/g, "");
+  return standardForm === standardForm.split("").reverse().join("");
 };
 
 // Do not edit below this line
